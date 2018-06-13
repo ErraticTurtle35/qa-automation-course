328ed4ca1e19b784b2c4da7444b59c3837b66b45
Gabriel Morales <gabriel.morales@swissbytes.ch>
2018-06-12 20:20:02 -0400

    Add new failing test

--- python-tdd/Int2Lcd/TestIntToLcd.py
+++ python-tdd/Int2Lcd/TestIntToLcd.py
@@ -4,6 +4,7 @@ from Int2Lcd import Int2Lcd
 
 zero = [" _ ", "| |", "|_|"]
 one = [" ", " |", " |"]
+two = ["_", "_|", "|_"]
 
 
 class TestInt2Lcd(unittest.TestCase):
@@ -14,3 +15,7 @@ class TestInt2Lcd(unittest.TestCase):
     def test_conversion_from_one_to_lcd(self):
         result = Int2Lcd().convert(1)
         self.assertListEqual(one, result)
+
+    def test_conversion_from_two_to_lcd(self):
+        result = Int2Lcd().convert(2)
+        self.assertListEqual(two, result)
