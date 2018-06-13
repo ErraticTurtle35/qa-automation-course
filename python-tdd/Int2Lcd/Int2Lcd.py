5005f286fe6c039aa6cb4106bb64ca5677cfd3df
Gabriel Morales <gabriel.morales@swissbytes.ch>
2018-06-12 20:11:35 -0400

    Add first refactor for convert method from Int2Lcd

--- python-tdd/Int2Lcd/Int2Lcd.py
+++ python-tdd/Int2Lcd/Int2Lcd.py
@@ -1,3 +1,7 @@
+numbers = (0, [" _ ", "| |", "|_|"])
+
+
 class Int2Lcd:
     def convert(self, number):
-        return [" _ ", "| |", "|_|"]
+        if number == numbers[0]:
+            return [" _ ", "| |", "|_|"]
