f485e0dd71039f1a659012275370b975de001562
Gabriel Morales <gabriel.morales@swissbytes.ch>
2018-06-12 20:13:52 -0400

    Add second falling test for test one

--- python-tdd/Int2Lcd/TestIntToLcd.py
+++ python-tdd/Int2Lcd/TestIntToLcd.py
@@ -9,3 +9,7 @@ class TestInt2Lcd(unittest.TestCase):
     def test_conversion_from_zero_to_lcd(self):
         result = Int2Lcd().convert(0)
         self.assertListEqual(zero, result)
+
+    def test_conversion_from_one_to_lcd(self):
+        result = Int2Lcd().convert(1)
+        self.assertListEqual(zero, result)
