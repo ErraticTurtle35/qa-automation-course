7256e34d0f60190584ea3d2e0569758dcf535b4c
Gabriel Morales <gabriel.morales@swissbytes.ch>
2018-06-12 20:39:30 -0400

    Add test coverage to the rest of natural numbers

--- python-tdd/Int2Lcd/TestIntToLcd.py
+++ python-tdd/Int2Lcd/TestIntToLcd.py
@@ -5,6 +5,13 @@ from Int2Lcd import Int2Lcd
 zero = [" _ ", "| |", "|_|"]
 one = [" ", " |", " |"]
 two = [" _", " _|", "|_"]
+three = [" _", " _|", " _|"]
+four = ["   ", "|_|", "  |"]
+five = [" _", "|_ ", " _|"]
+six = [" _", "|_", "|_|"]
+seven = [" _ ", "  |", "  |"]
+eight = [" _ ", "|_|", "|_|"]
+nine = [" _ ", "|_|", "  |"]
 
 
 class TestInt2Lcd(unittest.TestCase):
@@ -19,3 +26,31 @@ class TestInt2Lcd(unittest.TestCase):
     def test_conversion_from_two_to_lcd(self):
         result = Int2Lcd().convert(2)
         self.assertListEqual(two, result)
+
+    def test_conversion_from_three_to_lcd(self):
+        result = Int2Lcd().convert(3)
+        self.assertListEqual(three, result)
+
+    def test_conversion_from_four_to_lcd(self):
+        result = Int2Lcd().convert(4)
+        self.assertListEqual(four, result)
+
+    def test_conversion_from_five_to_lcd(self):
+        result = Int2Lcd().convert(5)
+        self.assertListEqual(five, result)
+
+    def test_conversion_from_six_to_lcd(self):
+        result = Int2Lcd().convert(6)
+        self.assertListEqual(six, result)
+
+    def test_conversion_from_seven_to_lcd(self):
+        result = Int2Lcd().convert(7)
+        self.assertListEqual(seven, result)
+
+    def test_conversion_from_eight_to_lcd(self):
+        result = Int2Lcd().convert(8)
+        self.assertListEqual(eight, result)
+
+    def test_conversion_from_nine_to_lcd(self):
+        result = Int2Lcd().convert(9)
+        self.assertListEqual(nine, result)
