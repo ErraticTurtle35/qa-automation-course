a0a294539005027a059fde22cf2f523b187f6cc1
Gabriel Morales <gabriel.morales@swissbytes.ch>
2018-06-13 20:32:07 -0400

    Add failing  smoke test

--- python-tdd/Int2Lcd/main.py
+++ python-tdd/Int2Lcd/main.py
@@ -1,7 +1,7 @@
 from Int2Lcd import Int2Lcd
 
 if __name__ == "__main__":
-    lcd__display = Int2Lcd().display(0)
+    lcd__display = Int2Lcd().display(1234567890)
     [print(column) for column in lcd__display]
     [print(s) for s in Int2Lcd().convert(0)]
     [print(s) for s in Int2Lcd().convert(1)]
