b0a5e00667d3bb56709c357b4fd58dff1a289765
Gabriel Morales <gabriel.morales@swissbytes.ch>
2018-06-21 17:15:08 -0400

    Refactor method convert

--- python-tdd/Int2Lcd/Int2Lcd.py
+++ python-tdd/Int2Lcd/Int2Lcd.py
@@ -21,43 +21,10 @@ class Int2Lcd:
 
     def convert(self, number):
         number_pattern = self.patterns[number].copy()
-        if number_pattern[0][0].isspace() and number_pattern[0][1] == "_" and number_pattern[0][2].isspace():
-            number_pattern[0] = " " + "".join(["_" for i in range(self.width)]) + " "
-        elif number_pattern[0][0].isspace() and number_pattern[0][1].isspace() and number_pattern[0][2].isspace():
-            number_pattern[0] = " " + "".join([" " for i in range(self.width)]) + " "
-
-        if number_pattern[1][0] == "|" and number_pattern[1][1].isspace() and number_pattern[1][2] == "|":
-            number_pattern[1] = "|" + "".join([" " for i in range(self.width)]) + "|"
-        elif number_pattern[1][0].isspace() and number_pattern[1][1].isspace() and number_pattern[1][2] == "|":
-            number_pattern[1] = " " + "".join([" " for i in range(self.width)]) + "|"
-        elif number_pattern[1][0].isspace() and number_pattern[1][1] == "_" and number_pattern[1][2] == "|":
-            number_pattern[1] = " " + "".join(["_" for i in range(self.width)]) + "|"
-        elif number_pattern[1][0] == "|" and number_pattern[1][1] == "_" and number_pattern[1][2] == "|":
-            number_pattern[1] = "|" + "".join(["_" for i in range(self.width)]) + "|"
-        elif number_pattern[1][0] == "|" and number_pattern[1][1] == "_" and number_pattern[1][2].isspace():
-            number_pattern[1] = "|" + "".join(["_" for i in range(self.width)]) + " "
-
-        if number_pattern[2][0] == "|" and number_pattern[2][1] == "_" and number_pattern[2][2] == "|":
-            number_pattern[2] = "|" + "".join(["_" for i in range(self.width)]) + "|"
-        elif number_pattern[2][0].isspace() and number_pattern[2][1].isspace() and number_pattern[2][2] == "|":
-            number_pattern[2] = " " + "".join([" " for i in range(self.width)]) + "|"
-        elif number_pattern[2][0] == "|" and number_pattern[2][1] == "_" and number_pattern[2][2].isspace():
-            number_pattern[2] = "|" + "".join(["_" for i in range(self.width)]) + " "
-        elif number_pattern[2][0].isspace() and number_pattern[2][1] == "_" and number_pattern[2][2] == "|":
-            number_pattern[2] = " " + "".join(["_" for i in range(self.width)]) + "|"
-
+        self._update_width(number_pattern)
         if self.height > 1:
-            new_number_pattern = []
-            for pattern in number_pattern:
-                if "|" in pattern:
-                    for repetition in range(self.height):
-                        if repetition != self.height - 1:
-                            new_number_pattern.append(pattern.replace("_", " "))
-                        else:
-                            new_number_pattern.append(pattern)
-                else:
-                    new_number_pattern.append(pattern)
-            number_pattern = new_number_pattern
+            new_number_pattern = self._update_height_of_rows(number_pattern)
+            return new_number_pattern
         return number_pattern
 
     @property
@@ -79,3 +46,49 @@ class Int2Lcd:
     def __init__(self, width=None, height=None):
         self._width = width
         self._height = height
+
+    def _update_width(self, number_pattern):
+        self._update_width_of_the_first_row(number_pattern)
+        self._update_width_of_the_middle_row(number_pattern)
+        self._update_width_of_the_lower_row(number_pattern)
+
+    def _update_height_of_rows(self, number_pattern):
+        new_number_pattern = []
+        for pattern in number_pattern:
+            if "|" in pattern:
+                for repetition in range(self.height):
+                    if repetition != self.height - 1:
+                        new_number_pattern.append(pattern.replace("_", " "))
+                    else:
+                        new_number_pattern.append(pattern)
+            else:
+                new_number_pattern.append(pattern)
+        return new_number_pattern
+
+    def _update_width_of_the_lower_row(self, number_pattern):
+        if number_pattern[2][0] == "|" and number_pattern[2][1] == "_" and number_pattern[2][2] == "|":
+            number_pattern[2] = "|" + "".join(["_" for i in range(self.width)]) + "|"
+        elif number_pattern[2][0].isspace() and number_pattern[2][1].isspace() and number_pattern[2][2] == "|":
+            number_pattern[2] = " " + "".join([" " for i in range(self.width)]) + "|"
+        elif number_pattern[2][0] == "|" and number_pattern[2][1] == "_" and number_pattern[2][2].isspace():
+            number_pattern[2] = "|" + "".join(["_" for i in range(self.width)]) + " "
+        elif number_pattern[2][0].isspace() and number_pattern[2][1] == "_" and number_pattern[2][2] == "|":
+            number_pattern[2] = " " + "".join(["_" for i in range(self.width)]) + "|"
+
+    def _update_width_of_the_middle_row(self, number_pattern):
+        if number_pattern[1][0] == "|" and number_pattern[1][1].isspace() and number_pattern[1][2] == "|":
+            number_pattern[1] = "|" + "".join([" " for i in range(self.width)]) + "|"
+        elif number_pattern[1][0].isspace() and number_pattern[1][1].isspace() and number_pattern[1][2] == "|":
+            number_pattern[1] = " " + "".join([" " for i in range(self.width)]) + "|"
+        elif number_pattern[1][0].isspace() and number_pattern[1][1] == "_" and number_pattern[1][2] == "|":
+            number_pattern[1] = " " + "".join(["_" for i in range(self.width)]) + "|"
+        elif number_pattern[1][0] == "|" and number_pattern[1][1] == "_" and number_pattern[1][2] == "|":
+            number_pattern[1] = "|" + "".join(["_" for i in range(self.width)]) + "|"
+        elif number_pattern[1][0] == "|" and number_pattern[1][1] == "_" and number_pattern[1][2].isspace():
+            number_pattern[1] = "|" + "".join(["_" for i in range(self.width)]) + " "
+
+    def _update_width_of_the_first_row(self, number_pattern):
+        if number_pattern[0][0].isspace() and number_pattern[0][1] == "_" and number_pattern[0][2].isspace():
+            number_pattern[0] = " " + "".join(["_" for i in range(self.width)]) + " "
+        elif number_pattern[0][0].isspace() and number_pattern[0][1].isspace() and number_pattern[0][2].isspace():
+            number_pattern[0] = " " + "".join([" " for i in range(self.width)]) + " "
